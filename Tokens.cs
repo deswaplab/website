74217f27b3ef4da9c53e7b9863f12153c4494221
Joey <joey.xf@gmail.com>
2024-01-02 12:08:41 +0800

    add mint balance helper text and balance link

--- Tokens.cs
+++ Tokens.cs
@@ -17,26 +17,40 @@ public class TokenPair
     public string NftAddress { get; set; } = "";
 
     public int ChainId { get; set; } = 0;
+
+    public string EtherScanHost {get; set;} = "";
+
+    // Like: https://sepolia.etherscan.io/token/0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9?a=0x7e727520B29773e7F23a8665649197aAf064CeF1
+    public string GetEtherScanTokenBalanceUrl(string contractAddress, string userAddress)
+    {
+        return EtherScanHost + "/token/" + contractAddress + "?a=" + userAddress;
+    }
 }
 
 public class TokenPairs
 {
-    public IList<TokenPair> Inner {get; set;}
+    public IList<TokenPair> Inner { get; set; }
 
-    public TokenPairs() {
+    public TokenPairs()
+    {
         Inner = [
-            new TokenPair{BaseAssetName = "WETH", BaseAssetAddress = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9", BaseAssetDecimals = 18, QuoteAssetName = "USDC", QuoteAssetAddress = "0x6CcB30b54Bf2B1Cf47E093B92aECCE404F9824Cd", QuoteAssetDecimals = 6, NftAddress = "0xEB0bDd73ADB291B245CDB48c650Cf684607D2033", ChainId = 11155111 },
+            new TokenPair{
+                BaseAssetName = "WETH", 
+                BaseAssetAddress = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9", 
+                BaseAssetDecimals = 18, 
+                QuoteAssetName = "USDC", 
+                QuoteAssetAddress = "0x6CcB30b54Bf2B1Cf47E093B92aECCE404F9824Cd", 
+                QuoteAssetDecimals = 6, 
+                NftAddress = "0xEB0bDd73ADB291B245CDB48c650Cf684607D2033", 
+                ChainId = 11155111,
+                EtherScanHost = "https://sepolia.etherscan.io" 
+            },
         ];
     }
 
-    public TokenPair? FindByPair(string name) {
+    public TokenPair? FindByPair(string name)
+    {
         var res = Inner.FirstOrDefault(item => item.BaseAssetName + "/" + item.QuoteAssetName == name);
         return res;
-    }
-    public static IList<TokenPair> GetTokenPairs()
-    {
-        return [
-                new TokenPair{BaseAssetName = "WETH", BaseAssetAddress = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9", BaseAssetDecimals = 18, QuoteAssetName = "USDC", QuoteAssetAddress = "0x6CcB30b54Bf2B1Cf47E093B92aECCE404F9824Cd", QuoteAssetDecimals = 6, NftAddress = "0xEB0bDd73ADB291B245CDB48c650Cf684607D2033", ChainId = 11155111 },
-        ];
     }
 }
