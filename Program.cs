0f7f5fdd137a7890350e2f6f7a6f59e83dcbcbc5
Joey <joey.xf@gmail.com>
2024-01-05 16:03:17 +0800

    add sample collections page

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddAuthorizationCore();
-builder.Services.AddSingleton<StateContainer>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IMetamaskInterop, MetamaskBlazorInterop>();
