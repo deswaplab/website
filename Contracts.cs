a0e799ba3f14010f86568d323dff28ae8ddb16b0
Joey <joey.xf@gmail.com>
2024-04-29 17:39:05 +0800

    remove support for polygon mumbai testnet

--- Contracts.cs
+++ Contracts.cs
@@ -76,19 +76,6 @@ public static class NetworkConfig
         RpcUrl = "https://rpc.ankr.com/eth_sepolia",
     };
 
-    public static NetworkCore PolygonMumbai { get; } = new()
-    {
-        Name = "Pylogon Mumbai",
-        InnerName = "polygon_mumbai",
-        ChainId = 80001,
-        EtherscanHost = "https://mumbai.polygonscan.com",
-        OpenseaHost = "https://testnets.opensea.io/assets/mumbai",
-        OpenseaApiHost = "https://testnets-api.opensea.io/api/v2/chain/mumbai",
-        Logo = "polygon_logo.svg",
-        IsTestNet = true,
-        RpcUrl = "https://rpc-mumbai.polygon.technology",
-    };
-
     public static NetworkCore MoonBaseAlpha { get; } = new()
     {
         Name = "MoonBase Alpha",
@@ -174,7 +161,6 @@ public static class NetworkConfig
 
     public static readonly IList<NetworkCore> Inner = [
         EthereumSepolia,
-        PolygonMumbai,
         MoonBaseAlpha,
         // SolanaNeonDev,
         MantaPacificSepolia,
@@ -199,7 +185,6 @@ public static class ContractConfig
 {
     public static readonly Dictionary<NetworkCore, ERC20Contract> Weth = new() {
         { NetworkConfig.EthereumSepolia,     new ERC20Contract("WETH", "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9", 18) },
-        { NetworkConfig.PolygonMumbai,       new ERC20Contract("WETH", "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", 18) },
         { NetworkConfig.MoonBaseAlpha,       new ERC20Contract("WETH", "0xD909178CC99d318e4D46e7E66a972955859670E1", 18) },
         { NetworkConfig.MantaPacificSepolia, new ERC20Contract("WETH", "0x199d1a27684106dC3Deb673115fc0fc9cf6af287", 18) },
         { NetworkConfig.MantleSepolia,       new ERC20Contract("WETH", "0x17f711A85D359cBF0224a017d8e3dd7A29c9932E", 18) },
@@ -207,7 +192,6 @@ public static class ContractConfig
 
     public static readonly Dictionary<NetworkCore, ERC20Contract> Tusdc = new() {
         { NetworkConfig.EthereumSepolia,     new ERC20Contract("TUSDC", "0xb53ff72177708cd6A643544B7caD9a2768aCC8E5", 6) },
-        { NetworkConfig.PolygonMumbai,       new ERC20Contract("TUSDC", "0xb53ff72177708cd6A643544B7caD9a2768aCC8E5", 6) },
         { NetworkConfig.MoonBaseAlpha,       new ERC20Contract("TUSDC", "0x0EBB63025caE604fd9230A2fdc811a84394A7861", 6) },
         { NetworkConfig.MantaPacificSepolia, new ERC20Contract("TUSDC", "0x7EDaec7d9db23D4Af62eA2FF71F7104279347f14", 6) },
         { NetworkConfig.MantleSepolia,       new ERC20Contract("TUSDC", "0x238285119Ad0842051B4a46A9428139d30869B55", 6) },
@@ -237,7 +221,6 @@ public static class ContractConfig
 
     public static readonly Dictionary<NetworkCore, NFTContract> Barter = new() {
         { NetworkConfig.EthereumSepolia,     new NFTContract("Barter", "0x677dfb01d6F3C30EA1Ce4497775AB94EAB699981") },
-        { NetworkConfig.PolygonMumbai,       new NFTContract("Barter", "0x677dfb01d6F3C30EA1Ce4497775AB94EAB699981") },
         { NetworkConfig.MoonBaseAlpha,       new NFTContract("Barter", "0xb9afd20d69CEEdC249E32649ef10015af6C05c3F") },
         { NetworkConfig.MantaPacificSepolia, new NFTContract("Barter", "0xBAA68A83fa8a0eb7c1144EE275350c3C4F5B61b0") },
         { NetworkConfig.MantleSepolia,       new NFTContract("Barter", "0x5A4cA8E3Cd655F5Ec72a353A40aD82159A37FC58") },
@@ -245,7 +228,6 @@ public static class ContractConfig
 
     public static readonly Dictionary<NetworkCore, NFTContract> Lottery = new() {
         { NetworkConfig.EthereumSepolia,     new NFTContract("Lottery", "0xF1a162C2d43ea2d1f2Af764cC6E564EaC2A7dC01") },
-        { NetworkConfig.PolygonMumbai,       new NFTContract("Lottery", "0xF1a162C2d43ea2d1f2Af764cC6E564EaC2A7dC01") },
         { NetworkConfig.MoonBaseAlpha,       new NFTContract("Lottery", "0x5cAE158176D8c30F7038b127Da9FC845B916a99A") },
         { NetworkConfig.MantaPacificSepolia, new NFTContract("Lottery", "0x86cF9996b7a8aF46E74A9FBbd28b149a2f7F33e2") },
         { NetworkConfig.MantleSepolia,       new NFTContract("Lottery", "0xD40D28361Ef1c9FB6E672E0905548CAdD4d70AA1") },
@@ -253,7 +235,6 @@ public static class ContractConfig
 
     public static readonly Dictionary<NetworkCore, NFTContract> RedEnvelope = new() {
         { NetworkConfig.EthereumSepolia,     new NFTContract("RedEnvelope", "0x629391548a7D4f9848e2F9377aFFC5540F4158A7") },
-        { NetworkConfig.PolygonMumbai,       new NFTContract("RedEnvelope", "0x629391548a7D4f9848e2F9377aFFC5540F4158A7") },
         { NetworkConfig.MoonBaseAlpha,       new NFTContract("RedEnvelope", "0xE7cd7918C6f402cf29779DAC7F4Ba582F7448016") },
         { NetworkConfig.MantaPacificSepolia, new NFTContract("RedEnvelope", "0xCeA52909295CAABEFF8Dc7820bDF9Bf8403683D9") },
         { NetworkConfig.MantleSepolia,       new NFTContract("RedEnvelope", "0x8106ac53d21B3180471a19D359d610A19c15aD9B") },
@@ -261,7 +242,6 @@ public static class ContractConfig
 
     public static readonly Dictionary<NetworkCore, NFTContract> Roulette = new() {
         { NetworkConfig.EthereumSepolia,     new NFTContract("Roulette", "0xaC0f2E348A8501349c366cEfe49658918CaccA01") },
-        { NetworkConfig.PolygonMumbai,       new NFTContract("Roulette", "0xaC0f2E348A8501349c366cEfe49658918CaccA01") },
         { NetworkConfig.MoonBaseAlpha,       new NFTContract("Roulette", "0xCeA52909295CAABEFF8Dc7820bDF9Bf8403683D9") },
         { NetworkConfig.MantaPacificSepolia, new NFTContract("Roulette", "0x508A1f2c538Ae9f1BF1D923f0d90ff3372c9720B") },
         { NetworkConfig.MantleSepolia,       new NFTContract("Roulette", "0x7C38B98cc50fcA103529253F0Be1C079f7E39b82") },
